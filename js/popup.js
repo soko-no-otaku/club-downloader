98dcc98271b6d52e20dcfbcb6ca0d840e40ef244
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-07 02:14:30 +0900

    ソートを数値順に修正

--- js/popup.js
+++ js/popup.js
@@ -19,7 +19,12 @@ function makeList(m3u8List, titleList) {
 }
 
 window.onload = function () {
-    var m3u8List = chrome.extension.getBackgroundPage().m3u8List.sort().reverse();
+    var m3u8List = chrome.extension.getBackgroundPage().m3u8List.sort(function (a, b) {
+        var re = /^https:\/\/dc\.miovp\.com\/wrap_m3u8\/ac-amu\/1\/(\d+)\/.+$/;
+        var num_a = Number(a.match(re)[1]);
+        var num_b = Number(b.match(re)[1]);
+        return num_b - num_a;
+    });
     var titleList = chrome.extension.getBackgroundPage().titleList;
 
     makeList(m3u8List, titleList);
